533128a5058163ae18b08270464b46a779d3e001
Carpa88 <olenka_311@mail.ru>
2023-10-01 14:20:50 +0200

    added ThemeProvider. theme is default

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from './Header/Header';
 
-const App = () => (
-    <>
+const App = () => {
+  const defaultTheme = createTheme();
+ 
+  return (
+    <ThemeProvider theme={defaultTheme}>
       <CssBaseline />  
       <Header />
       <h1>Hello, World!</h1>
-    </>
-  )
+    </ThemeProvider>
+  )}
 
 export default App
