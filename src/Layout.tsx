3a156e53f1d41496198a15cff9f6e41b3def7e6c
Carpa88 <olenka_311@mail.ru>
2023-10-06 11:03:43 +0200

    Deleted paddings for container

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import Footer from './elements/Footer';
 import About from './About/About';
 
 const Layout = () => (
-	<Container maxWidth="lg" sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
+	<Container maxWidth="lg" sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column', p: 0}}>
 		<Header />
 		<About />
 		<Footer />
