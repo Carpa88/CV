a3546a2c6ec6e6595f24e3a87c78c54e3ca01975
Carpa88 <olenka_311@mail.ru>
2023-10-09 14:48:52 +0200

    Delete pl for Typography

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -3,7 +3,7 @@ import Section from "../elements/Section";
 
 const About = () => (
 	<Section title='about'>
-		<Typography variant='body1' id='about' sx={{ maxWidth: { sx: '100%', md: '80%' }, px: 2 }}>
+		<Typography variant='body1' id='about' sx={{ maxWidth: { sx: '100%', md: '80%' } }}>
 			Highly motivated and adaptable Software Engineer with a strong background 12 years in IT (10 years as BA and 2 years as Developer). Seeking opportunities to utilize my technical expertise and problem-solving skills to contribute to innovative projects and drive software development success.
 		</Typography>
 	</Section>
