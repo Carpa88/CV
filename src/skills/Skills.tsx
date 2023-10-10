790e7b3bb0ad6f7f47f81f93bc92bc57a9babad4
Carpa88 <olenka_311@mail.ru>
2023-10-10 12:41:14 +0200

    Added Collaps to Skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,19 +1,58 @@
-import { Grid, Card, Typography, alpha } from "@mui/material";
+import React from "react";
+import { alpha, Grid, Card, Typography, Button, CardActions, CardContent, IconButton, Collapse } from "@mui/material";
 import { skillsData } from "./constants";
 import Section from "../elements/Section";
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
 
-const Skills = () => (
-	<Section title='Skills'>
-		<Grid container sx={{ display: 'flex', flexDirection: 'row' }}>
-			{skillsData.map(item => (
-				<Grid item xs={12} sm={6} md={4} key={item}>
-					<Card sx={theme =>({ display: 'flex', mr: 4, mb: 4, px: 3, py: 2, backgroundColor: alpha(theme.palette.secondary.light, 0.5) })}>
-						<Typography>{item}</Typography>
-					</Card>
-				</Grid>
-			))}
-		</Grid>
-	</Section>
-)
+type IExpands = Record<string, boolean>
+
+const Skills = () => {
+	const expandedSkillsData: IExpands =
+		skillsData
+			.map(({ title }) => (title))
+			.reduce((obj, cur) => {obj[cur] = false; return obj }, {} as IExpands)
+
+	const [expanded, setExpanded] = React.useState<IExpands>(expandedSkillsData);
+	
+	console.log('start', expanded);
+	
+	const handleExpandClick = (title: string) => {
+		setExpanded({...expanded, [title]: !expanded[title]});
+		console.log('onClick', expanded);
+	};
+	return (
+		<Section title='Skills'>
+			<Grid container spacing={2} sx={{ display: 'flex', flexDirection: 'row' }}>
+				{skillsData.map(({title, preview, text}) => (
+					<Grid item xs={12} sm={6} md={4} key={title}>
+						<Card sx={{ display: 'flex', flexDirection: 'column', maxWidth: 345, backgroundColor: theme => alpha(theme.palette.common.black, 0.05) }}>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div">
+									{title}
+								</Typography>
+								<Typography variant="body2" color="text.secondary" sx={{ height: '81px' }}>
+									{preview}
+								</Typography>
+							</CardContent>
+							<CardActions sx={{ display: 'flex', justifyContent: 'space-between', mt: 'auto' }}>
+								<Button size="small">Share</Button>
+								<IconButton size="small" onClick={()=> handleExpandClick(title)}>
+									{text && (expanded[title] ? <ExpandLess /> : <ExpandMore />)}
+								</IconButton>
+							</CardActions>
+							<Collapse in={expanded[title]} timeout="auto" unmountOnExit>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary" >
+										{text}
+									</Typography>
+								</CardContent>
+							</Collapse>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Section>
+	)
+}
 
 export default Skills;
--- src/skills/constants.ts
+++ src/skills/constants.ts
@@ -1,1 +1,33 @@
-export const skillsData: string[] = ['HTML', 'CSS', 'Javascript', 'ReactJS', 'TypeScript', 'Git', 'Vite']
+export const skillsData: { title: string; preview: string; text?: string }[] = [
+	{
+		title: 'HTML',
+		preview: "HTML is not a programming language. It's a markup language. In fact, that is the technology's name: HyperText Markup Language.",
+	},
+	{
+		title: 'CSS',
+		preview: 'Cascading Style Sheets (CSS) is a stylesheet language that instructs the browser how we want our HTML to appear; it is not a programming language.',
+	}, 
+	{
+		title: 'Javascript',
+		preview: 'JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there',
+		text: "and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area.",
+	},
+	{
+		title: 'ReactJS',
+		preview: 'React.js is an open-source JavaScript library used for making reactive user interfaces specifically for single-page websites. This JavaScript framework was developed',
+		text: "by Jordan Walke, a software engineer working at Facebook, which was released on 29th May 2013. Many people believe that React is a programming language but it is a library that is one of the most used in web development especially for front-end purposes. Developers can use React to build large web projects that can change data on the web page without the need of reloading the site.",
+	},
+	{
+		title: 'TypeScript',
+		preview: 'TypeScript is a strongly typed, object-oriented, compiled programming language that builds on JavaScript. It is a superset of the JavaScript language, designed to give you',
+		text: "better tooling at any scale.",
+	},
+	{
+		title: 'Git',
+		preview: "Git is the most widely used modern version control system in the world today. ",
+	},
+	{
+		title: 'Vite',
+		preview: "Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects.",
+	},
+]
