3cb7c62716e802bff73ea5398f6ee3f416a4afa2
Carpa88 <olenka_311@mail.ru>
2023-11-07 13:31:50 +0100

    Changed link to my linkidin page

--- src/elements/Copyright.tsx
+++ src/elements/Copyright.tsx
@@ -1,10 +1,11 @@
 import {Link, Typography, TypographyProps} from '@mui/material';
+import {CV_LINK} from './constants';
 
 const Copyright = (props?: TypographyProps) => (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" href='#'>
-            CV
+        <Link color="inherit" href={CV_LINK}>
+            Resume
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
