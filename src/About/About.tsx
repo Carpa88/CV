fa81e75c340b03a394dffe113fcd56ae1a751740
Carpa88 <olenka_311@mail.ru>
2023-11-07 13:52:36 +0100

    Added text to the page About

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,11 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Typography } from "@mui/material";
 import Section from "../elements/Section";
 
 const About = () => (
 	<Section title='about'>
-		<Typography variant='body1' id='about' sx={{ maxWidth: { sx: '100%', md: '80%' } }}>
-			Highly motivated and adaptable Software Engineer with a strong background 12 years in IT (10 years as BA and 2 years as Developer). Seeking opportunities to utilize my technical expertise and problem-solving skills to contribute to innovative projects and drive software development success.
-		</Typography>
+		<Typography variant='h3'>A Journey through 12 Years of Professional Growth: my Career Story</Typography>
+
+		In the dynamic world of Information Technology, where change is the only constant, I stand out as an experienced and adaptable Software Engineer. With a remarkable 12-year background in the field, I have carved a path of success that has left an indelible mark on the industry.
+
+		<Typography variant="h3">The Early Years</Typography>
+
+		I embarked on this exciting journey in 2011 as a Business Analyst at Generic Solution, Softon Way, Novacom. Over the course of a decade, My exhibited an unerring ability to adapt, learn, and excel. Stakeholder interviews and requirements gathering became second nature, and translating complex project needs into effective developer tasks was my forte. Facilitating team meetings, sprint planning sessions, and conducting rigorous testing ensured that projects were delivered efficiently and flawlessly.
+
+		<Typography variant="h3">Evolution into Software Engineering</Typography>
+
+		In 2021, I took a pivotal step by transitioning to the role of Front-End Software Engineer at Novacom. The move was characterized by swift learning and a hunger for new challenges. In this role, I demonstrated versatility by working on various projects:
+
+		<Typography variant="h3">Inter Project - JavaScript, Rest API, React, CSS, MUI, TypeScript, Webpack: </Typography>
+		Rapidly acquired expertise in ReactJS and TypeScript.
+		Constructed a feature-rich news section for an internal portal, amplifying content management capabilities.
+		Collaborated with designers to enhance portal sections with innovative features.
+		
+		<Typography variant="h3">Commercial Project - JavaScript, Rest API, React, CSS, MUI, TypeScript, Vite:</Typography>
+		Implemented role-based access control, empowering secure access management.
+		Created dynamic forms and tables, optimizing data presentation.
+		Managed tasks efficiently by breaking them into subtasks with timeframes.
+		
+		<Typography variant="h3">Inter Project - JavaScript, Rest API, React, CSS, MUI, TypeScript, Vite:</Typography>
+		Developed a comprehensive website showcasing backend developers' work.
+		The website featured multiple pages, forms, and data listings, becoming a valuable resource for the company team.
+		My ability to work under tight deadlines, pay meticulous attention to detail, and bring an analytical approach to problem-solving has been instrumental in project success.
+
+		<Typography variant="h3">The Academic Foundation</Typography>
+
+		The journey began with a Bachelor of Science in Information Systems and Technologies Specialist (Ecology) from the INTERNATIONAL SAKHAROV ENVIRONMENTAL INSTITUTE in Minsk, Belarus. The academic background provided a solid foundation for my career, combining technical expertise with an understanding of environmental systems.
+
+		<Typography variant="h3">Language Skills</Typography>
+
+		I'm not just a tech enthusiast but also a linguist. Proficient in English, I have achieved a B1 level of proficiency. In addition to English, I have a basic understanding of Polish (A1) and is a native speaker of Russian.
+				
 	</Section>
 )
 
